65841edd4b13b655c9e807ec474485125a5d0638
gagan-28deep <gagan_28_deep@outlook.com>
2024-07-13 10:31:08 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,36 @@
+var num = 10
+console.log(num);
+console.log(typeof num);
+
+let str= "hello"
+console.log(str);
+console.log(typeof str);
+
+const isTrue = true
+console.log(isTrue);
+console.log(typeof isTrue);
+
+
+// task 4
+
+const arr = [1, 2, 3, 4, 5]
+console.log(arr);
+console.log(typeof arr);
+
+const obj = {
+    name: "John",
+    age: 30
+}
+
+console.log(obj);
+console.log(typeof obj);
+
+// task 5
+
+let num1 = 10
+num1 = 20
+console.log(num1)
+
+const num2 = 10
+num2 = 20
+console.log(num2)
