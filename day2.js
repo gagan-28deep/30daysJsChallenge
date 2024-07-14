30995e20d919cbfeaef0d5d36092f2a9e9bfba43
gagan-28deep <gagan_28_deep@outlook.com>
2024-07-14 12:10:43 +0530

    day 2 done

---
+++ day2.js
@@ -0,0 +1,61 @@
+// Activity 1
+
+let num1 = 5
+let num2 = 10
+
+console.log(num1 + num2);
+
+// Subtract
+
+console.log(num1 - num2);
+
+// Multiply
+console.log(num1 * num2);
+
+// Divide
+console.log(num1 / num2);
+
+// Remainder
+console.log(num1 % num2);
+
+
+// Activity 2
+
+num1 +=20
+console.log(num1);
+
+num2 -=10
+console.log(num2);
+
+
+// Activity 3
+
+let num3 = 65
+let num4 = 45
+
+console.log(num3 > num4);
+console.log(num3 < num4);
+
+
+console.log(num3 >= num4);
+console.log(num3 <= num4);
+
+console.log(num3 == num4);
+
+// Activity 4
+
+let n = num3 && num4
+console.log(n); 
+
+let m = num3 || num4
+console.log(m);
+
+let p = !num3
+console.log(p);
+
+// Activity 5
+
+let a = -10
+let isPositive = a > 0
+
+console.log(isPositive);
