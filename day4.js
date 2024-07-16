5310ced5856c38372677fd8ba732185c2a60d939
gagan-28deep <gagan_28_deep@outlook.com>
2024-07-16 11:19:10 +0530

    day 4 loops

---
+++ day4.js
@@ -0,0 +1,61 @@
+// Activity 1
+
+// for (let i = 1 ; i<11 ; i++){
+//     console.log("i" , i)
+// }
+// for (let i = 1 ; i<11 ; i++){
+//     console.log(`5 * ${i} = ${5*i}`)
+// }
+
+// Activity 2
+
+// Sum of numbers using while loop
+
+// let ans = 0
+// let i = 1
+
+// while (i <= 10) {
+//     ans = ans + i
+//     i++
+// }
+// console.log(ans)
+
+// let num = 10
+
+// while (num > 0) {
+//     console.log(num);
+//     num--;
+// }
+
+// Activity 3
+
+// Do while
+
+// let i = 1
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5)
+
+// fact of a number using do while loop
+
+// let fact = 1
+// let num = 4
+
+// do {
+//     fact = fact * num
+//     num--
+// } while (num > 0)
+// console.log(fact)
+
+// Activity 4
+
+// Print a triangle
+
+for (let i = 1; i <= 5; i++) {
+  let str = "";
+  for (let j = 1; j <= i; j++) {
+    str += "*";
+  }
+  console.log(str);
+}
